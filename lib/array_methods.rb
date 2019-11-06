38502344ce27c031081704d208d2511c170da3b3
Emilee Sudweeks <emilee.sudweeks@wework.com>
2019-11-06 00:35:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,13 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort!
 end
 
 def using_reverse(array)
-
+  expect(using_reverse(array).last.to eq("wow"))
 end
 
 def using_first(array)
